aedbb561f782b44a516ba9b93921e718f8eb993a
Richu Joy <80971275+Richu077@users.noreply.github.com>
2021-05-28 09:18:55 +0530

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,44 +1,22 @@
-  
-from __future__ import division, print_function
-# coding=utf-8
-
-import pickle
-
-# Keras
-from keras.models import load_model
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template,request, redirect, url_for
 from gevent.pywsgi import WSGIServer
 
-MODEL_PATH = 'models/sample.h5'
 
-
-
-
-model = load_model(MODEL_PATH)
-print('Model loaded. Start serving...')
-
-
-
-
-
-
-from flask import Flask, render_template, url_for, jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
+
+    if request.method =='POST':
+        question1 = request.form['searchBox1']
+        question2 = request.form['searchBox2']
+    else:
+        question1 = request.args.get('searchBox1')
+        question2 = request.args.get('searchBox2')
     return render_template('home.html')
 
 
-@app.route('/result', methods=['POST'])
-def result():
-    text1 = request.form['searchBox']
-    return render_template('result.html', result1= 'The query is : > {}' .format(text1))   
-    
-    
 
-if __name__== '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
